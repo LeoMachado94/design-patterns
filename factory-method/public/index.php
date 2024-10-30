f49a495c601391f541ff659039abb8787e7ccc01
Leonardo Machado <leo94.machado@gmail.com>
2024-10-30 17:18:06 -0300

    wip

--- factory-method/public/index.php
+++ factory-method/public/index.php
@@ -3,4 +3,5 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $truck = \FactoryMethod\Models\Truck::factory(3);
 $airplane = \FactoryMethod\Models\Airplane::factory(9);
-dump(json_encode($truck), json_encode($airplane));
+
+dd($truck, $airplane);
